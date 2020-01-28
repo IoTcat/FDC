caa6dabc2094842d22334245134fb147134c270f
iotcat <i@iotcat.me>
2020-01-28 22:48:00 +0000

    Committed by cron-git from Yimian-PC

--- script/fdc_startup.bat
+++ script/fdc_startup.bat
@@ -1,2 +1,1 @@
-@echo off
-pm2 ls
+pm2 start $$$$ --name fdc --watch & pm2 ls
--- script/startup.bat
+++ script/startup.bat
@@ -9,5 +9,12 @@ git config user.usr %usr%
 git push
 pm2 start update.js --name fdc
 pm2 save
-echo "pm2 start %cd%\update.js --name fdc&pm2 ls" > "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\fdc.bat"
+COPY /Y script\fdc_startup.bat "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\"
+setlocal enabledelayedexpansion
+set fn="C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\fdc_startup.bat"
+(for /f "tokens=*" %%i in (%fn%) do (
+set s=%%i
+set s=!s:$$$$=%cd%!
+echo !s!))>temp1126.txt
+move /y temp1126.txt "%fn%"
 npm i -g npm
