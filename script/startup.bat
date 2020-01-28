0d028a448967d7174deda69adef32bcd48b00249
iotcat <i@iotcat.me>
2020-01-28 23:06:30 +0000

    Committed by cron-git from Yimian-PC

--- script/startup.bat
+++ script/startup.bat
@@ -9,13 +9,7 @@ git push
 pm2 start update.js --name fdc
 pm2 save
 COPY /Y script\fdc_startup.bat tmp.bat
-setlocal enabledelayedexpansion
-set fn=tmp.bat
-(for /f "tokens=*" %%i in (%fn%) do (
-set s=%%i
-set s=!s:$$$$=%cd%!
-echo !s!))>temp1126.txt
-move /y temp1126.txt "%fn%"
+start test.bat
 npm i -g npm
 echo Setup finished!!!
 pause
