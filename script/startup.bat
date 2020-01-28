de56057b6510fd5b53cf9ccf92f7614d2c675dd4
iotcat <i@iotcat.me>
2020-01-28 21:57:00 +0000

    Committed by cron-git from Yimian-PC

--- script/startup.bat
+++ script/startup.bat
@@ -3,3 +3,9 @@
 cd /d "%~dp0"
 copy /Y fdc_startup.bat "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
 pm2 start update.js --name fdc
+cd ..
+set /p email=Git Email:
+set /p usr=Git Username:
+git config user.email %email%
+git config user.usr %usr%
+git pull
