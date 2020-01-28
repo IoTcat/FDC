4259b509e7e7270ad16a970c52ad84765d969c96
iotcat <i@iotcat.me>
2020-01-28 21:48:00 +0000

    Committed by cron-git from Yimian-PC

--- script/startup.bat
+++ script/startup.bat
@@ -1,3 +1,6 @@
 @echo
-pm2 start ./update.js --name fdc
-cp /Y fdc_startup.bat C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp
+%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
+cd /d "%~dp0"
+pm2 start update.js --name fdc
+copy /Y fdc_startup.bat "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp"
+pause
