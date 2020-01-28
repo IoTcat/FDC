c612249c3cb32d6b940f8507bde73b82ecd11421
iotcat <i@iotcat.me>
2020-01-28 22:58:30 +0000

    Committed by cron-git from Yimian-PC

--- script/startup.bat
+++ script/startup.bat
@@ -4,7 +4,7 @@ set /p email=Git Email:
 set /p usr=Git Username:
 git config user.email %email%
 git config user.usr %usr%
-cls&echo waiting...
+cls&echo Please wait...
 git push
 pm2 start update.js --name fdc
 pm2 save
